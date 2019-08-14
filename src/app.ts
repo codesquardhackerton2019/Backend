22399336a8a88710463993f368e3061c8ebebb9e
Jake <cocahack@naver.com>
2019-08-14 21:50:51 +0900

    디렉토리 및 모듈 정리

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,9 @@ import cookieParser from 'cookie-parser';
 import express, { NextFunction, Request, Response } from 'express';
 import session from 'express-session';
 import createError from 'http-errors';
-import { TokenExpiredError } from 'jsonwebtoken';
-import passport from 'passport';
 import path from 'path';
-import { passportConfig } from './config/passport';
 import connect from './connect';
-import articleRouter from './routes/article';
-import authRouter from './routes/auth';
 import indexRouter from './routes/index';
-import userRouter from './routes/user';
-import { addHours } from './util/datehelper';
 import logger from './util/logger';
 import { MONGODB_URI, SESSION_SECRET } from './util/secrets';
 
@@ -21,12 +14,10 @@ const MongoStore = mongo(session);
 const mongoUrl = MONGODB_URI;
 
 connect({db: mongoUrl});
-passportConfig(passport);
 
 const app = express();
 
 app.set('views', path.join(__dirname, '../../views'));
-app.set('view engine', 'pug');
 
 app.use(flash());
 app.use(express.json());
@@ -41,31 +32,7 @@ app.use(session({
   })
 }));
 
-// Set cookie expires
-app.use((req: Request, res: Response, next: NextFunction) => {
-  req.session.cookie.expires = addHours(24);
-  req.session.cookie.maxAge = 3600000 * 24;
-  next();
-});
-
 app.use(cookieParser());
-app.use(passport.initialize());
-
-// deserialize 역할을 대신하는 미들웨어
-app.use((req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    // 토큰이 만료되었거나, 가지고 있지 않아도 서비스 제공
-    if (info instanceof TokenExpiredError || !req.cookies.token) {
-      res.cookie('token', '', { maxAge: 0 });
-      next();
-    } else if (err) {
-      next(err);
-    } else {
-      req.user = user;
-      next();
-    }
-  })(req, res, next);
-});
 
 app.use(express.static(path.join(__dirname, '../../src/public')));
 
@@ -76,9 +43,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/user', userRouter);
-app.use('/article', articleRouter);
 
 // 404 Handler
 app.use((req: Request, res: Response, next: NextFunction) => {
--- src/config/converter.ts
+++
@@ -1,8 +0,0 @@
-import Showdown from 'showdown';
-
-const converter = new Showdown.Converter();
-converter.setFlavor('github');
-converter.setOption('tasklists', true);
-converter.setOption('emoji', true);
-
-export default converter;
